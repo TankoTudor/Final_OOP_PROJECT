0bb7f93c191151bfd04c880c1c5b730b137b505c
Alex <meraalex2001@gmail,com>
2022-01-20 06:13:05 +0200

    added shopping cart model for use in user interface at a later date (it is not finished)

---
+++ Models/Cart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Final_OOP_PROJECT.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int IdCart { get; set; }
+        public System.DateTime CheckOutTime { get; set; }
+        public int IdUtilizator { get; set; }
+        public int IdGame { get; set; }
+        public string DenumJoc { get; set; }
+        public int PretJoc { get; set; }
+       
+    }
+}
